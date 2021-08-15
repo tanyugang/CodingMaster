53e41d47f9ae7bf8ee711a05a82e5c035d99b504
tanyugang <tanyugang.com@gmail.com>
2021-08-15 20:23:20 +0800

    新增第45话代码

---
+++ 第45话/list.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAXNAME 32              //最大姓名长度
+#define GPERROR -1
+#define SUCESS  0
+
+typedef struct student
+{
+    unsigned int code;
+    char name[MAXNAME];
+    struct student *next;
+}Student;
+
+typedef struct stulist
+{
+    Student *head;
+    int count;
+}StuList;
+
+Student *createStudent()
+{
+    Student *stu = (Student *)malloc(sizeof(Student));
+    if(stu != NULL)
+    {
+        stu->code = 0;
+        memset(stu->name, 0, MAXNAME);
+        stu->next = NULL;
+        return stu;
+    }
+    return NULL;
+}
+int main(int argc, char* argv[])
+{
+    StuList stul;
+    Student* stu;
+    stul.head = createStudent();
+    if(stul.head == NULL)
+        return GPERROR;
+    stul.count = 1;
+    stu = stul.head;
+    scanf("%d,%s", &stu->code, stu->name);
+    stu->next = createStudent();
+    if(stu->next == NULL)
+        return GPERROR;
+    stul.count++;
+    stu = stu->next;
+    scanf("%d,%s", &stu->code, stu->name);
+    stu = stul.head;
+    for(int i = 0; i < stul.count; i++)
+    {
+        printf("StuList[%d]`s code is %d, name is %s\n", i, stu->code, stu->name);
+        stu = stu->next;
+    }
+    return SUCESS;
+}
